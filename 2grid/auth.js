38575040b34d145b78aa19e666c87b07dcbcb546
DIMIWU <43118996+DIMIWU@users.noreply.github.com>
2019-09-18 20:28:07 -0400

    123

--- 2grid/auth.js
+++ 2grid/auth.js
@@ -6,5 +6,52 @@ const message = document.getElementById("login-message");
 loginButton.onclick = function(event) {
 //	message.textContent = emailInput.value + " has logged in :)";
 	
-	firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+	const promise = firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value);
+    promise.catch(function(error){
+        message.textContent = error.message;
+    });
 };
+
+
+
+/* auth state */
+
+const displayName = document.getElementById("user-name");
+
+
+firebase.auth().onAuthStateChanged(function(user){
+//    console.log("user",yser.displayName);
+    
+    if(user){
+        document.body.classList.add('auth');
+        displayName.textContent = "Welcome," + user.displayName;
+    }
+    else{
+        document.body.classList.remove('auth');
+        displayName.textContent ="";
+    }
+    
+});
+
+
+
+
+/* log out*/
+const logoutButton = document.getElementById("logout-button");
+logoutButton.onclick = function() {
+	firebase.auth().signOut();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
