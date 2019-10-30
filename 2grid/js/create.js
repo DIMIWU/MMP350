dd68814af478367a20e656814ab3a0763f3c1429
Dimi Wu <565105022@qq.com>
2019-10-30 16:13:41 -0400

    change123

--- 2grid/js/create.js
+++ 2grid/js/create.js
@@ -1,40 +1,28 @@
+const userInput = document.getElementById("username");
 const emailInput = document.getElementById("email");
-const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
 const createButton = document.getElementById("create-button");
 const errorMessage = document.getElementById("error-message");
 
-
-
-createButton.onclick = function(event) {
-    
-    const promise =
-	firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
-    
-    promise.catch(function(error){
+createButton.onclick = function() {
+    const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
+    promise.catch(function(error) {
         errorMessage.textContent = error.message;
     });
     
-//    promise.then(function(){
-//        location.herf = "index.html";
-//    });
-    
-    
-    promise.then(function(credential(){
-        createUser(credential.ueser.uid);
-    
-      });
-});
-
-fuction createUser(id){
-    const db = firebase.database();
-    const ref = db.ref("users").child(id);
-    const promise = red.update({
-        displaName: userInput.value
+    promise.then(function(credential) {
+        createUser(credential.user.uid);
     });
-    
-    promise.then(funcyion(){
-                 location.herf ="index.html";
-    });
-    
 };
+
+function createUser(uid) {
+    const db = firebase.database();
+    const ref = db.ref("users").child(uid);
+    const promise = ref.update({
+        displayName: userInput.value
+    });
+    
+    promise.then(function() {
+        location.href = "index.html";
+    });
+}
